0ce6d24422647e3ee75649524a38a295c5b6aa9b
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-01-14 11:27:33 -0500

    Clase 1

---
+++ Clase 1/index.js
@@ -0,0 +1,33 @@
+//comentario de una sola linea
+
+/*
+comentario
+de
+varias
+lineas
+*/
+
+// let edad = 45; //forma recomendable de definir variables
+// var nombre = "andres"; //forma no recomendada de definir variables
+// let apellido = 'Ruiz';
+// const CURSO = "JavaScript";
+
+// nombreBonitoGrande => CamelCase => para javascript
+// nombre_bonito_grande => SnakeCase
+// NombreBonitoGrande => PascalCase
+
+// let numeroUno = 5;
+// let numeroDos = 10;
+// let resultadoSuma = numeroUno + numeroDos;
+// let resultadoResta = numeroUno - numeroDos;
+
+// console.log("aca el resultado de la suma", resultadoSuma);
+// console.log("aca el resultado de la resta", resultadoResta);
+
+// let saludo = "Hola";
+// let nombre = "andres";
+// let saludoCompleto = saludo + " " + nombre;
+// console.log(saludoCompleto);
+let nombre = prompt("Ingrese su nombre");
+let mensaje = "Hola " + nombre;
+alert(mensaje);
