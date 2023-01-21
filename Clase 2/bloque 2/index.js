a72a1116053c874dbb21742588358765cf0358e6
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-01-21 12:01:16 -0500

    Clase 2

--- Clase 2/bloque 2/index.js
+++ Clase 2/bloque 2/index.js
@@ -59,3 +59,88 @@ for(desde; hasta; actualizacion){
 
 //   console.log(i);
 // }
+
+/* 
+estructura del ciclo while
+
+while(condicion){
+  codigo a repetir siempre que la condicion sea verdadera
+}
+*/
+
+// let repetir = true;
+
+// while(repetir){
+//   console.log("hola");
+// }
+
+// let usuario = prompt("Ingrese su usuario");
+
+// while(usuario != "andres"){
+//   alert("usuario incorrecto");
+//   usuario = prompt("Ingrese su usuario");
+// }
+
+// alert("Bienvenido andres");
+
+// let entrada = prompt("Ingrese un dato");
+
+// while(entrada != "ESC"){
+//   alert(`ingresaste ${entrada}`);
+//   entrada = prompt("Ingrese un dato");
+// }
+
+// let repetir = false;
+
+// do{
+//   console.log("entramos");
+// }while(repetir);
+
+// while(repetir){
+//   console.log("entramos");
+// }
+
+/* 
+
+estructura del switch
+
+switch(valor){
+  case valor1:
+    codigo a ejecutar en caso de que valor sea igual a valor1
+    break;
+
+  case valor2:
+    codigo a ejecutar en caso de que valor sea igual a valor1
+    break;
+
+  .
+  .
+  .
+
+  default:
+    codigo a ejecutar en caso de que el valor no coincida con ninguno de los anteriores valores
+    break;
+}
+*/
+
+let nombre = prompt("Ingrese el nombre");
+
+while(nombre != "ESC"){
+  switch(nombre){
+    case "ANA":
+      alert("Hola Ana");
+      break;
+
+    case "JUAN":
+      alert("Hola Juan");
+      break;
+
+    default:
+      for(let i = 0; i < 3; i++){
+        alert("Quién eres???");
+      }
+      break;
+  }
+
+  nombre = prompt("Ingrese el nombre");
+}
