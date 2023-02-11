c904dcc5f8d22d792fd4b41ac440a22bf03a5fba
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-02-11 10:55:20 -0500

    Clase 5

---
+++ Clase 5/parte 2/main.js
@@ -0,0 +1,11 @@
+let boton = document.getElementById("boton");
+
+// let evento = prompt("Ingrese el evento");
+// boton.addEventListener(evento, () => console.log("Fuego!!!"));
+
+// botononclick = () =>. console.log("Fuego!!!");
+// const saludar = (nombre) => {
+//   alert(`Hola ${nombre}`);
+// };
+
+// boton.addEventListener("click", () => saludar("camila"));
